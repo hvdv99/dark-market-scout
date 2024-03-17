4e8d932a14849a52d373097dc3890bc6fbfb3c80
huub <huubvandevoort@home.nl>
2024-03-17 14:05:26 +0100

    added configuration methods to runner.py

--- runner/runner.py
+++ runner/runner.py
@@ -5,9 +5,11 @@ from config import constants as c
 crawl_scout = Crawler()
 crawl_scout.set_seed(c.seed)
 crawl_scout.set_cookies(c.cookies)
-crawl_scout.set_max_pages_to_crawl(200)
+crawl_scout.set_max_pages_to_crawl(3)
+crawl_scout.set_request_interval(3)
+crawl_scout.set_user_agent_behaviour(15)  # after how many requests the user agent will be replaced.
 crawl_scout.set_request_timing_behaviour('random')
-crawl_scout.set_resource_dir('redbull market')
+crawl_scout.set_resource_dir('kerberos')
 
 # start crawling
 crawl_scout.crawl()
