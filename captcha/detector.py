c4477228aae710d8d4549f32a25e5c96276cf567
huub <huubvandevoort@home.nl>
2024-04-05 19:37:54 +0200

    Implemented Bayes as Captcha classifier, tests passing, need more data

--- captcha/detector.py
+++ captcha/detector.py
@@ -1,3 +1,11 @@
+import os
+from bs4 import BeautifulSoup as bs
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import ComplementNB
+
+
 class CaptchaDetector:
     """
     CaptchaDetector class is used to detect if a scraped html page contains a captcha instead of the expected content.
@@ -16,6 +24,8 @@ class CaptchaDetector:
     def __init__(self):
         self.captcha_fingerprints = ["Strike", "Strikes", "Security breach detected", "Captcha",
                                      "ddos 2-factor-protection"]
+        self.vectorizer = CountVectorizer()
+        self.model = self._train_bayes()
 
     def detect_captcha(self, html: str) -> bool:
         """This method detects if a scraped html page contains a captcha instead of the expected content.
@@ -28,7 +38,7 @@ class CaptchaDetector:
         # Currently, it just checks if the html contains any of the predefined captcha fingerprints.
         # But we want to have a pipeline of some sort that combines multiple methods to detect captchas.
         # First, need to gather more data on how captchas are implemented on different websites.
-        return self._find_fingerprint(html)
+        return self._bayes_decide(html)
 
     def _find_fingerprint(self, html: str) -> bool:
         """Finds if a Captcha fingerprint is present in the html content"""
@@ -36,3 +46,60 @@ class CaptchaDetector:
             if fingerprint.lower() in html.lower():
                 return True
         return False
+
+    def _bayes_decide(self, html: str) -> bool:
+        clean_html = self._parse_html(html=html)
+        vectorized = self.vectorizer.transform([clean_html])
+        prediction = self.model.predict(vectorized)
+        if prediction == 'captcha':
+            return True
+        return False
+
+    def _parse_html(self, html: str) -> str:
+        page = bs(html, 'html.parser').text
+        page = page.split('\n')
+
+        while '' in page:
+            page.remove('')
+        page = ' '.join(page)
+        page = page.split(' ')
+
+        while '' in page:
+            page.remove('')
+        page = ' '.join(page)
+
+        return page.lower().replace("\t", "").replace("\\", "").strip()
+
+    def _load_data(self):
+        train_data_dir = os.path.join('..', 'captcha', 'training-data')
+        captcha_files = [os.path.join(train_data_dir, 'captcha', f) for f in
+                         os.listdir(os.path.join(train_data_dir, 'captcha')) if f.endswith('.html')]
+        non_captcha_files = [os.path.join(train_data_dir, 'non_captcha', f) for f in
+                             os.listdir(os.path.join(train_data_dir, 'non_captcha')) if f.endswith('.html')]
+
+        all_files = [captcha_files, non_captcha_files]
+
+        result = list()
+        for i, label in enumerate(['captcha', 'non-captcha']):
+            for file in all_files[i]:
+                with open(file, 'r') as f:
+                    text = self._parse_html(f.read())
+                    result.append((text, label))
+
+        X, y = list(), list()
+        for data, label in result:
+            X.append(data)
+            y.append(label)
+        return X, y
+
+    def _train_bayes(self):
+        docs, labels = self._load_data()
+
+        X = self.vectorizer.fit_transform(docs)
+
+        X_train, X_test, y_train, y_test = train_test_split(X, labels,
+                                                            test_size=0.2, random_state=42, shuffle=True)
+        model = ComplementNB()
+        model.fit(X_train, y_train)
+
+        return model
